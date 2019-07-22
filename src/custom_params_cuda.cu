23a23b3d46d8dbe8a32d66519f73ebb8d019e506
Ian Ruh <iruh@lanl.gov>
2019-07-22 07:53:04 -0600

    Remove test kernel

--- src/custom_params_cuda.cu
+++ src/custom_params_cuda.cu
@@ -12,13 +12,8 @@ __constant__ Real custom_params[100];
 /* \fn void Copy_Custom_Params(Real* parameters)
 * \brief Copy the passed parameters to constant memory on the GPU */
 void Copy_Custom_Params(Real* parameters) {
-    printf("Custom Parameters: [%d, %d, %d, %d, %d, ...]\n", parameters[0], parameters[1], parameters[2], parameters[3], parameters[4]);
+    printf("Custom Parameters: [%f, %f, %f, %f, %f, ...]\n", parameters[0], parameters[1], parameters[2], parameters[3], parameters[4]);
     cudaMemcpyToSymbol(custom_params, parameters, 100*sizeof(Real));
-    testFunc<<<1,1>>>();
-}
-
-__global__ void testFunc() {
-    printf("Custom Parameter GPU: %f\n", custom_params[0]);
 }
 
 #endif
